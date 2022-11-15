123881207bdbc48d3baf521ab094eca8e2e7b645
Nico Sigal <nicolassigal@orange.fr>
2022-11-15 11:18:20 +0100

    Realisateur class creation

--- TP3/myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
+++ TP3/myfilmlist/src/main/java/com/ensta/myfilmlist/model/Realisateur.java
@@ -1,8 +1,52 @@
+package com.ensta.myfilmlist.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
 public class Realisateur {
-      long id
-      String nom
-      String prenom
-      LocalDate dateNaissance
-      List<Film> filmRealises
-      boolean celebre
+      private long id;
+      private String nom;
+      private String prenom;
+      private LocalDate dateNaissance;
+      private List<Film> filmRealises;
+      private boolean celebre;
+
+      public long getId() {
+            return id;
+      }
+      public void setId(long id) {
+            this.id = id;
+      }
+      public String getNom() {
+            return nom;
+      }
+      public void setNom(String nom) {
+            this.nom = nom;
+      }
+      public String getPrenom() {
+            return prenom;
+      }
+      public void setPrenom(String prenom) {
+            this.prenom = prenom;
+      }
+      public LocalDate getDateNaissance() {
+            return dateNaissance;
+      }
+      public void setDateNaissance(LocalDate dateNaissance) {
+            this.dateNaissance = dateNaissance;
+      }
+      public List<Film> getFilmRealises() {
+            return filmRealises;
+      }
+      public void setFilmRealises(List<Film> filmRealises) {
+            this.filmRealises = filmRealises;
+      }
+      public boolean isCelebre() {
+            return celebre;
+      }
+      public void setCelebre(boolean celebre) {
+            this.celebre = celebre;
+      }
+
+
 }
