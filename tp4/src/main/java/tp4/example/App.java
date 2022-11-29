8a0f5ec9b89239f60e28627ea0063eda650e0c69
Nico Sigal <nicolassigal@orange.fr>
2022-11-29 09:02:55 +0100

    Commit 29 Nov

---
+++ tp4/src/main/java/tp4/example/App.java
@@ -0,0 +1,13 @@
+package tp4.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
